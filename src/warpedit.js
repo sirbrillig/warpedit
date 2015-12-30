12f91c9566d1d61d6a4d14565eedf31cffa9bd96
Payton Swick <payton@foolord.com>
2015-12-30 18:48:39 -0500

    Add comments to explain the very complicated callInitialActions

--- src/warpedit.js
+++ src/warpedit.js
@@ -33,27 +33,34 @@ export default React.createClass( {
 	},
 
 	callInitialActions( newProps ) {
+		// when we start, we may be in one of the following states:
 		debug( 'calling initial actions' );
+		// - no site or no post in the url: clear the state and stop
 		if ( ! newProps.params.site || ! newProps.params.post ) {
 			debug( 'no site or post specified. resetting state.' );
 			return store.dispatch( clearState() );
 		}
 		const hashParams = querystring.parse( newProps.location.hash.substr( 1 ) );
+		// - auth token in the url: save the auth token and the site and post
 		if ( hashParams.access_token ) {
 			debug( 'got oauth token', hashParams.access_token );
 			store.dispatch( saveToken( hashParams.access_token, newProps.params.site, newProps.params.post ) );
 		}
+		// - site in the url does not match site in the store: clear the store, fetch a new token, and stop
 		if ( newProps.params.site !== store.getState().site ) {
 			debug( 'requesting new authentication token for', newProps.params.site );
 			return store.dispatch( getAuthForNewSite( newProps.params.site, newProps.params.post ) );
 		}
+		// - site and post in the url but not in the store: save those to the store
 		if ( ! store.getState().site || ! store.getState().post ) {
 			store.dispatch( saveSite( newProps.params.site, newProps.params.post ) );
 		}
+		// - no auth token in the store: fetch a new token and stop
 		if ( ! store.getState().authToken ) {
 			debug( 'requesting authentication token for', newProps.params.site );
 			return store.dispatch( getAuthFromServer( newProps.params.site ) );
 		}
+		// - auth token in the store but no markup fetched: fetch the markup
 		if ( ! store.getState().markup ) {
 			debug( 'requesting initial markup' );
 			store.dispatch( fetchInitialMarkup() );
