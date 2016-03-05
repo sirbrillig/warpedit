a602d863cb172aa4157d28e8ecb9a62235db43c9
Payton Swick <payton@foolord.com>
2016-03-04 20:12:56 -0500

    Decode html entities in editor-panel

--- src/editor-panel.js
+++ src/editor-panel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import noop from 'lodash.noop';
+import { decode } from 'ent';
 
 export default React.createClass( {
 	displayName: 'EditorPanel',
@@ -22,12 +23,12 @@ export default React.createClass( {
 
 	getInitialState() {
 		return {
-			content: this.props.content,
+			content: decode( this.props.content ),
 		}
 	},
 
 	componentWillReceiveProps( nextProps ) {
-		this.setState( { content: nextProps.content } );
+		this.setState( { content: decode( nextProps.content ) } );
 	},
 
 	componentDidUpdate() {
