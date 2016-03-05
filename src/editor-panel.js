e4fdd003ee5bd82d5cf3a09613063d12342f9543
Payton Swick <payton@foolord.com>
2016-03-04 19:54:21 -0500

    Use Draft.js as the editor-panel

--- src/editor-panel.js
+++ src/editor-panel.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import noop from 'lodash.noop';
+import { Editor, EditorState, ContentState } from 'draft-js';
 
 export default React.createClass( {
 	displayName: 'EditorPanel',
@@ -21,33 +21,41 @@ export default React.createClass( {
 	},
 
 	getInitialState() {
+		const contentState = ContentState.createFromText( this.props.content );
 		return {
 			content: this.props.content,
+			editorState: EditorState.createWithContent( contentState ),
 		}
 	},
 
 	componentWillReceiveProps( nextProps ) {
-		this.setState( { content: nextProps.content } );
-	},
-
-	componentDidUpdate() {
-		if ( this.props.active ) {
-			ReactDOM.findDOMNode( this.editField ).focus();
-		}
-	},
-
-	handleChange( event ) {
-		const newContent = event.target.value;
-		this.setState( { content: newContent }, () => {
-			this.props.onChange( newContent );
+		const contentState = ContentState.createFromText( nextProps.content );
+		console.log( 'updating state' );
+		this.setState( {
+			content: nextProps.content,
+			editorState: EditorState.createWithContent( contentState ),
 		} );
 	},
 
+	componentDidUpdate() {
+		if ( this.props.active && this.editField ) {
+			this.editField.focus();
+		}
+	},
+
+	handleChange( editorState ) {
+		const content = editorState.getCurrentContent().getPlainText();
+		console.log( 'changed', content );
+		this.setState( { editorState, content } );
+	},
+
 	render() {
+		// TODO: we cannot pass the current content up and back down again, so we
+		// need a done button inside of this component, I guess
 		const classes = classNames( 'warpedit-editor-panel', { 'is-active': this.props.active } );
 		return (
 			<div className={ classes }>
-				<textarea rows="10" cols="80" onChange={ this.handleChange } value={ this.state.content } ref={ ( input ) => this.editField = input }/>
+				<Editor editorState={ this.state.editorState } onChange={ this.handleChange } ref={ input => this.editField = input } />
 			</div>
 		);
 	}
